be548b62d7166ce48338f998a848b0e5652208d8
John Wang <johnjinghuawang@gmail.com>
2022-05-16 22:49:01 -0700

    add cors setting

--- backend/src/main/java/SparkServer.java
+++ backend/src/main/java/SparkServer.java
@@ -56,6 +56,20 @@ public class SparkServer {
             .build();
     FirebaseApp defaultApp = FirebaseApp.initializeApp(options);
 
+    // Server side cors setting
+    options("/*", (request, response) -> {
+        String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+          if (accessControlRequestHeaders != null) {
+              response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+          }
+          String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+          if (accessControlRequestMethod != null) {
+              response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+          }
+          return "OK";
+    });
+    before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
     /*
       --------------------------------------- USER RELATED -----------------------------------------------
     */
