c1877b86a9c3b7d493f3ec37d4956437fe30f4b2
John Wang <johnjinghuawang@gmail.com>
2022-05-17 16:38:01 -0700

    change json mapping to reflect backend change

--- frontend/lib/json/event.dart
+++ frontend/lib/json/event.dart
@@ -1,54 +1,27 @@
-// class EventInfo {
-//   final int eventId;
-//   final String eventName;
-//   final String description;
-//   final double latitude;
-//   final double longtitude;
-
-//   const EventInfo({
-//     required this.eventId,
-//     required this.eventName,
-//     required this.description,
-//     required this.latitude,
-//     required this.longtitude,
-//   });
-
-//   factory EventInfo.fromJson(Map<String, dynamic> json) {
-//     print(EventInfo(
-//       latitude: json['latitude'],
-//       longtitude: json ['longitude'],
-//       eventId: json['event_id'],
-//       eventName: json['event_name'],
-//       description: json['description']
-//     ).toString());
-//     return EventInfo(
-//       latitude: json['latitude'],
-//       longtitude: json ['longitude'],
-//       eventId: json['event_id'],
-//       eventName: json['event_name'],
-//       description: json['description']
-//     );
-//   }
-// }
+/// This is a Dart class the models detialed event information created by Host
+/// This will be a 1-1 mapping to the JSON data from backend API
 class EventInfo {
 
   final int eventId;
   final String eventCode;
   final String eventName;
   final String description;
-  final String hostId;
+  final String hostId; // the user id of the event creator
   final int isPublic; // 1 for public, 0 for private
   final int isDeleted; // 1 for deleted, 0 for not deleted
 
+  // event location info
   final String locationName;
   final double latitude;
   final double longitude;
+  final String address;
+
+  // number of participant
   final int maxParticipants;
   final int currNumParticipants;
 
   final String photoID;
   final String icon;
-  final String address;
 
   final String startTime;
   final String endTime;
@@ -83,8 +56,8 @@ class EventInfo {
       eventName: json['event_name'],
       description: json['description'],
       hostId: json['host_id'],
-      isPublic: json['isPublic'], 
-      isDeleted: json['isDeleted'],
+      isPublic: json['is_public'], 
+      isDeleted: json['is_deleted'],
 
       locationName: json['location_name'],
       latitude: json['latitude'],
@@ -92,7 +65,7 @@ class EventInfo {
       maxParticipants: json['max_participants'],
       currNumParticipants: json['curr_num_participants'],
 
-      photoID: json['photoID'],
+      photoID: json['photo_id'],
       icon: json['icon'],
       address: json['address'],
       startTime: json['start_time'],
